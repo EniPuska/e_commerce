995d3353e32ef5338ef90878bd4db7462230d84a
Eni Puska <eni.puska@gmail.com>
2018-04-14 16:21:34 +0300

    Add files via upload

---
+++ admin/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	$(".image_container").click(function(){
+		var $user_input;
+		return $user_input = confirm("Are you sure you want to delete this file?");
+		location.reload();
+	});
+});	
